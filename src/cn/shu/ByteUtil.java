06627c50ec0c7db35ae1e111de97b04dfad8ed1a
xingchen77 <493704481@qq.com>
2017-07-05 16:27:34 +0800

    初步解析

--- src/cn/shu/ByteUtil.java
+++ src/cn/shu/ByteUtil.java
@@ -2,6 +2,10 @@ package cn.shu;
 
 import cn.shu.entity.Message;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Date;
+
 public class ByteUtil {
 	public static String bytesToHexString(byte[] src){
 		StringBuilder stringBuilder = new StringBuilder("");
@@ -15,13 +19,31 @@ public class ByteUtil {
 			}
 		return stringBuilder.toString();
 		}
+	public static String jisuan(String string,double num){
+		double temp = Integer.parseInt(string,16)/num;
+		DecimalFormat decimalFormat=new DecimalFormat(".00");
+		return decimalFormat.format(temp);
+	}
 	public static Message contentChange(byte[] src){
-		Message message=new Message("1","2","4","5");
-		System.out.println(message.getId());
-		System.out.println(message.getTime());
-		System.out.println(message.getEnergy());
-		System.out.println(message.getPower());
-		return message;
+		//增加处理程序
+		String srcString = bytesToHexString(src);
+		String temp = srcString.substring(0,8);
+		String baseData="ffffe770";
+		if(baseData.equals(temp)){
+			String id = srcString.substring(20,28);
+			String data = DataSaver.sd.format(new Date());
+			String energy = jisuan(srcString.substring(52,60),4.0);//00002a75
+			String power = jisuan(srcString.substring(48,52),1024.0);//04ea
+			Message message=new Message(id,data,energy,power);
+			System.out.println(message.getId());
+			System.out.println(message.getTime());
+			System.out.println(message.getEnergy());
+			System.out.println(message.getPower());
+			return message;
+		}else{
+			return null;
+		}
+
 	}
-	}
+}
 	
