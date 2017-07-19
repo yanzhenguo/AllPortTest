02858a21404714b0751e2d514fb67a790df60eb6
yanzhenguo <1053977845@qq.com>
2017-07-19 18:07:34 +0800

    删除无用文件

--- test/cn/shu/TestClass.java
+++ test/cn/shu/TestClass.java
@@ -1,26 +1,75 @@
 package cn.shu;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class TestClass {
 
 	public static void main(String[] args) throws IOException {
-		//String path=System.getProperty("test.txt");
-		//System.out.println(path);
-		//File f = new File(path);
-		//System.out.println(f.exists());
-		//InputStream in=TestClass.class.getClassLoader().getResourceAsStream("test.txt");
-		//String path = Thread.currentThread().getContextClassLoader().getResource("data").getPath();
-		//String path = TestClass.class.getClassLoader().getResource("data").getPath();
-		//String path = System.getProperty("user.dir");
-		//File f=new File(path+"\\test\\test.txt");
-		//String path = TestClass.class.getClassLoader().getResource("test.txt").getPath();
-		//File f = new File(path);
-		System.out.println((byte)0xFF);
+		Scanner in = new Scanner(System.in);
+		Long res;
+		int dimension_size = Integer.parseInt(in.nextLine().trim());
+		List<Long[]> dimList = new ArrayList<Long[]>();
 
+		for (int _dimension_i = 0; _dimension_i < dimension_size; _dimension_i++) {
+			Long[] dimension_item = new Long[5];
+			String dimItemStr = in.nextLine().trim();
+			String[] dimItemStrs = dimItemStr.split(" ");
+			for (int j = 0; j < 5; j++) {
+				dimension_item[j] = Long.parseLong(dimItemStrs[j]);
+			}
+			dimList.add(dimension_item);
+		}	
+		
+		res = guessMyPath(dimList);
+		System.out.println(res);
+
+	}
+	/**
+	 * 计算救所有同伴所需的燃料
+	 * @param dimList
+	 * @return
+	 */
+	public static Long guessMyPath(List<Long[]> dimList){
+		Long res = 0L;
+		Long[] current = {0L,0L,0L,0L,0L};  //保存当前位置坐标
+		List<Long[]> notArrive = dimList;   //保存未到达位置坐标		
+		for(int i=0;i<dimList.size();i++){  //依次救出每一位同伴
+			int miniIndex = getNearFriend(notArrive, current); 
+			res+=Math.abs((notArrive.get(miniIndex)[0]-current[0]))+Math.abs((notArrive.get(miniIndex)[1]-current[1]))+Math.abs((notArrive.get(miniIndex)[2]-current[2]))+
+					10*Math.abs((notArrive.get(miniIndex)[3]-current[3])) + 100*Math.abs((dimList.get(miniIndex)[4]-current[4]));
+			current = notArrive.get(miniIndex);			
+			notArrive.remove(miniIndex);
+			if(i==dimList.size()-1){ //返回原点
+				res+=Math.abs((notArrive.get(miniIndex)[0]))+Math.abs((notArrive.get(miniIndex)[1]))+Math.abs((notArrive.get(miniIndex)[2]))+
+						10*Math.abs((notArrive.get(miniIndex)[3])) + 100*Math.abs((dimList.get(miniIndex)[4]));
+			}
+		}
+		
+		return res;
+	}
+	/**
+	 * 找到离得最近的同伴,返回id
+	 * @param dimList
+	 * @return
+	 */
+	public static int getNearFriend(List<Long[]> dimList, Long[] current){
+		Long miniDistance = Long.MAX_VALUE;
+		int miniIndex = 0;
+		for(int j=0;j<dimList.size();j++){
+			Long distance = 0L;
+			for(int i=0;i<5;i++){
+				distance+=(dimList.get(j)[i]-current[i])^2;
+			}
+			if(distance<miniDistance){
+				miniDistance=distance;
+				miniIndex=j;
+			}
+		}
+		return miniIndex;
 	}
 
 }
--- test/cn/shu/TestGui.java
+++ test/cn/shu/TestGui.java
@@ -1,59 +1,30 @@
 package cn.shu;
 
-import java.awt.Color;
-
+import java.awt.*;
 import javax.swing.*;
 
 import cn.shu.gui.MainWindow;
 
 public class TestGui {
 	
-	private JFrame frame = new JFrame("监控");
-	public JTextArea tf = new JTextArea();
-	public JButton button1 =new JButton("清除日志");
-	public JPanel panel1 = new JPanel();
-	public JPanel panel2 = new JPanel();
+	private JFrame frame;
+	public JTextArea tf;
+	public JButton button1;
+	public JScrollPane scroll;
+	public JPanel panel1;
+	public JPanel panel2;
 	
 	public TestGui(){
-//		//this.setSize(800, 800);		
-//		frame.addWindowListener(new WindowAdapter(){
-//			@Override
-//			public void windowClosing(WindowEvent e) {				
-//				System.exit(0);
-//			}
-//			
-//		});
-//		button1.addActionListener(new ActionListener(){
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				
-//				tf.setText("\n");				
-//				
-//			}});
+		frame = new JFrame();
+		frame.setSize(1000,800);
+ 		frame.setLayout(new GridLayout(1,1));
+ 		tf = new JTextArea("hello",20,30);
+ 		scroll=  new JScrollPane(tf);
+ 		panel1 = new JPanel();
+ 		panel1.add(scroll);
+ 		panel1.setLayout(new FlowLayout());
+		frame.add(panel1);
 		
-		
-		frame.setSize(1200,1000);	
-		frame.setLayout(null);
-		button1.setSize(120, 30);
-		//button1.setBackground(Color.black);
-		button1.setLocation(10, 10);
-		//panel1.setSize(600, 100);
-//		panel1.setBackground(Color.black);
-//		panel1.setLayout(new BorderLayout());
-//		panel1.add(button1,BorderLayout.WEST);
-//		//panel1.add(tf,BorderLayout.WEST);
-//		panel2.setSize(600, 200);
-//		panel2.setBackground(Color.green);
-//		panel2.setLayout(new BorderLayout());
-//		panel2.add(tf, BorderLayout.NORTH);
-//		frame.add(panel1,BorderLayout.NORTH);
-//		frame.add(panel2, BorderLayout.SOUTH);
-		tf.setSize(1160, 880);
-		tf.setLocation(10, 50);
-		frame.add(button1);
-		frame.add(tf);
-		frame.setBackground(Color.blue);
-		//frame.pack();
 		frame.setVisible(true);
 	}
 	
